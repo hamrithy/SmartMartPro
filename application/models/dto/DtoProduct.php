261b76bc573a5038664d56ae2da7793c25fc1355
Tola <Tola@Tola-pc>
2015-07-31 11:59:51 +0700

    DtoProduct

--- application/models/dto/DtoProduct.php
+++ application/models/dto/DtoProduct.php
@@ -3,98 +3,109 @@
 		
 		private $productid;
 		private $categoryid;
+		private $thumbnail;
 		private $userid;
 		private $seotitle;
 		private $seodescription;
 		
-		private $entitle;
-		private $endescription;
-		private $khtitle;
-		private $khdescription;
+		private $languageid;
+		private $title;
+		private $cation;
+		private $descritption;
 		private $createdate;
 		
-		private $thumbnail;
 		
-
 		public function getProductid(){
 			return $this->productid;
 		}
-	
+		
 		public function setProductid($productid){
 			$this->productid = $productid;
 		}
-	
-		public function getEntitle(){
-			return $this->entitle;
+		
+		public function getCategoryid(){
+			return $this->categoryid;
 		}
-	
-		public function setEntitle($entitle){
-			$this->entitle = $entitle;
+		
+		public function setCategoryid($categoryid){
+			$this->categoryid = $categoryid;
 		}
-	
-		public function getEndescription(){
-			return $this->endescription;
-		}
-	
-		public function setEndescription($endescription){
-			$this->endescription = $endescription;
-		}
-	
-		public function getKhtitle(){
-			return $this->khtitle;
-		}
-	
-		public function setKhtitle($khtitle){
-			$this->khtitle = $khtitle;
-		}
-	
-		public function getKhdescription(){
-			return $this->khdescription;
-		}
-	
-		public function setKhdescription($khdescription){
-			$this->khdescription = $khdescription;
-		}
-	
-		public function getCreatedate(){
-			return $this->createdate;
-		}
-	
-		public function setCreatedate($createdate){
-			$this->createdate = $createdate;
-		}
-	
-		public function getSeotitle(){
-			return $this->seotitle;
-		}
-	
-		public function setSeotitle($seotitle){
-			$this->seotitle = $seotitle;
-		}
-	
-		public function getSeodescription(){
-			return $this->seodescription;
-		}
-	
-		public function setSeodescription($seodescription){
-			$this->seodescription = $seodescription;
-		}
-	
+		
 		public function getThumbnail(){
 			return $this->thumbnail;
 		}
-	
+		
 		public function setThumbnail($thumbnail){
 			$this->thumbnail = $thumbnail;
 		}
-	
+		
 		public function getUserid(){
 			return $this->userid;
 		}
-	
+		
 		public function setUserid($userid){
 			$this->userid = $userid;
 		}
+		
+		public function getSeotitle(){
+			return $this->seotitle;
+		}
+		
+		public function setSeotitle($seotitle){
+			$this->seotitle = $seotitle;
+		}
+		
+		public function getSeodescription(){
+			return $this->seodescription;
+		}
+		
+		public function setSeodescription($seodescription){
+			$this->seodescription = $seodescription;
+		}
+		
+		public function getLanguageid(){
+			return $this->languageid;
+		}
+		
+		public function setLanguageid($languageid){
+			$this->languageid = $languageid;
+		}
+		
+		public function getTitle(){
+			return $this->title;
+		}
+		
+		public function setTitle($title){
+			$this->title = $title;
+		}
+		
+		public function getCation(){
+			return $this->cation;
+		}
+		
+		public function setCation($cation){
+			$this->cation = $cation;
+		}
+		
+		public function getDescritption(){
+			return $this->descritption;
+		}
+		
+		public function setDescritption($descritption){
+			$this->descritption = $descritption;
+		}
+		
+		public function getCreatedate(){
+			return $this->createdate;
+		}
+		
+		public function setCreatedate($createdate){
+			$this->createdate = $createdate;
+		}
+		
+		
+		
+
 	
 	}
 
