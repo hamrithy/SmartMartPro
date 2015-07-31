97e6347f78984275bb3012de989114518342a936
Tola <Tola@Tola-pc>
2015-07-31 12:02:26 +0700

    DtoCategory

--- application/models/dto/DtoCategory.php
+++ application/models/dto/DtoCategory.php
@@ -2,86 +2,84 @@
 class DtoCategory{
 
 	private $categoryid;
-	private $entitle;
-	private $endescription;
-	private $khtitle;
-	private $khdescription;
-	private $seotitle;
-	private $seodescription;
 	private $ordering;
 	private $subof;
+	private $seotitle;
+	private $seodescription;
+	
+	private $languageid;
+	private $title;
+	private $description;
 	
 	
 	public function getCategoryid(){
 		return $this->categoryid;
 	}
-
+	
 	public function setCategoryid($categoryid){
 		$this->categoryid = $categoryid;
 	}
-
-	public function getEntitle(){
-		return $this->entitle;
-	}
-
-	public function setEntitle($entitle){
-		$this->entitle = $entitle;
-	}
-
-	public function getEndescription(){
-		return $this->endescription;
-	}
-
-	public function setEndescription($endescription){
-		$this->endescription = $endescription;
-	}
-
-	public function getKhtitle(){
-		return $this->khtitle;
-	}
-
-	public function setKhtitle($khtitle){
-		$this->khtitle = $khtitle;
-	}
-
-	public function getKhdescription(){
-		return $this->khdescription;
-	}
-
-	public function setKhdescription($khdescription){
-		$this->khdescription = $khdescription;
-	}
-
-	public function getSeotitle(){
-		return $this->seotitle;
-	}
-
-	public function setSeotitle($seotitle){
-		$this->seotitle = $seotitle;
-	}
-
-	public function getSeodescription(){
-		return $this->seodescription;
-	}
-
-	public function setSeodescription($seodescription){
-		$this->seodescription = $seodescription;
-	}
-
+	
 	public function getOrdering(){
 		return $this->ordering;
 	}
-
+	
 	public function setOrdering($ordering){
 		$this->ordering = $ordering;
 	}
-
+	
 	public function getSubof(){
 		return $this->subof;
 	}
-
+	
 	public function setSubof($subof){
 		$this->subof = $subof;
 	}
+	
+	public function getSeotitle(){
+		return $this->seotitle;
+	}
+	
+	public function setSeotitle($seotitle){
+		$this->seotitle = $seotitle;
+	}
+	
+	public function getSeodescription(){
+		return $this->seodescription;
+	}
+	
+	public function setSeodescription($seodescription){
+		$this->seodescription = $seodescription;
+	}
+	
+	public function getLanguageid(){
+		return $this->languageid;
+	}
+	
+	public function setLanguageid($languageid){
+		$this->languageid = $languageid;
+	}
+	
+	public function getTitle(){
+		return $this->title;
+	}
+	
+	public function setTitle($title){
+		$this->title = $title;
+	}
+	
+	public function getDescription(){
+		return $this->description;
+	}
+	
+	public function setDescription($description){
+		$this->description = $description;
+	}
+	
+	
+	
+	
+	
+	
 	
 }
