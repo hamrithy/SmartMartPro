75872015e180498f3d0305829e9943ffb816c083
darapenhchet <darapenhchet@gmail.com>
2015-07-31 08:29:29 +0700

    Remove Log in Language_loader

--- application/hooks/language_loader.php
+++ application/hooks/language_loader.php
@@ -22,13 +22,13 @@ class Language_loader {
     function init() {
         $this->CI->load->helper('language');
         $language = $this->CI->input->cookie('LANGUAGE');
-        log_message('debug','Language_loader...'.$this->languages[$language]);
+        //log_message('debug','Language_loader...'.$this->languages[$language]);
         if($language){
             $this->CI->lang->load('smartmart',$this->languages[$language]);
             log_message('debug','IF Language_loader...'.$this->languages[$language]);
         }else{
             $this->CI->lang->load('smartmart', $this->CI->config->item('language'));
-            log_message('debug','ELSE Language_loader...'.$this->languages[$language]);
+            //log_message('debug','ELSE Language_loader...'.$this->languages[$language]);
         }    
     }
 }
