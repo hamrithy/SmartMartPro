a27097aefb940cc2f9ad39a9ed9e1c4fb7eb2860
Tola <Tola@Tola-pc>
2015-07-31 15:54:46 +0700

    ProductDetail

--- application/models/dto/DtoProduct.php
+++ application/models/dto/DtoProduct.php
@@ -8,13 +8,6 @@
 		private $seotitle;
 		private $seodescription;
 		
-		private $languageid;
-		private $title;
-		private $cation;
-		private $descritption;
-		private $createdate;
-		
-		
 		public function getProductid(){
 			return $this->productid;
 		}
@@ -63,49 +56,7 @@
 			$this->seodescription = $seodescription;
 		}
 		
-		public function getLanguageid(){
-			return $this->languageid;
-		}
 		
-		public function setLanguageid($languageid){
-			$this->languageid = $languageid;
-		}
-		
-		public function getTitle(){
-			return $this->title;
-		}
-		
-		public function setTitle($title){
-			$this->title = $title;
-		}
-		
-		public function getCation(){
-			return $this->cation;
-		}
-		
-		public function setCation($cation){
-			$this->cation = $cation;
-		}
-		
-		public function getDescritption(){
-			return $this->descritption;
-		}
-		
-		public function setDescritption($descritption){
-			$this->descritption = $descritption;
-		}
-		
-		public function getCreatedate(){
-			return $this->createdate;
-		}
-		
-		public function setCreatedate($createdate){
-			$this->createdate = $createdate;
-		}
-		
-		
-		
-
 	
 	}
 
