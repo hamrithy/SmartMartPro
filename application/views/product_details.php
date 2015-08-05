c59d2cc858a059a0ce17422160eb835c7f420305
phengtola <tolapheng99@gmail.com>
2015-08-06 01:19:43 +0700

    r

--- application/views/product_details.php
+++ application/views/product_details.php
@@ -285,55 +285,8 @@
         <?php $this->load->view('_footer'); ?>
         <!-- END FOOTER SECTION -->
         
-       		  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-              <script src="http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js"></script>
        
-       		  <script type="text/javascript">
-                           $(function(){ 
-	                            $.ajax({
-					                type: "POST",
-					                url: '<?php  echo site_url()?>/product/lstRelateProduct/9',
-					                dataType: 'json',
-					                success: function(data){ 
-					                    $(".lstRelateProduct").empty();
-					                    $("#tmplLstRelateProduct").tmpl(data.lstRelateProduct).appendTo(".lstRelateProduct");
-					                     console.log("DATA:",data); 
-					                },
-					                error: function(data){
-					                    console.log("ERROR...");
-					                    console.log(data);
-					                }
-					            });
-
-                           });
-       		  </script>
-       
-       				<script type="text/x-jquery-tmpl" id="tmplLstRelateProduct">
-      					 <div class="oc-item">
-                                <div class="product iproduct clearfix">
-                                    <div class="product-image">
-                                        <a href="#"><img src="<?php echo base_url('public/style_front/images/shop/dress/1.jpg')?>" alt="Checked Short Dress"></a>
-                                        <a href="#"><img src="<?php echo base_url('public/style_front/images/shop/dress/1-1.jpg')?>" alt="Checked Short Dress"></a>
-                                        <div class="sale-flash">50% Off*</div>
-                                        <div class="product-overlay">
-                                            <a href="#" class="add-to-cart"><i class="icon-shopping-cart"></i><span> Add to Cart</span></a>
-                                            <a href="include/ajax/shop-item.html" class="item-quick-view" data-lightbox="ajax"><i class="icon-zoom-in2"></i><span> Quick View</span></a>
-                                        </div>
-                                    </div>
-                                    <div class="product-desc center">
-                                        <div class="product-title"><h3><a href="#">Checked Short Dress</a></h3></div>
-                                        <div class="product-price"><del>$24.99</del> <ins>$12.49</ins></div>
-                                        <div class="product-rating">
-                                            <i class="icon-star3"></i>
-                                            <i class="icon-star3"></i>
-                                            <i class="icon-star3"></i>
-                                            <i class="icon-star3"></i>
-                                            <i class="icon-star-half-full"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                       </script>  
+       				 
                             
 </body>
 </html>
