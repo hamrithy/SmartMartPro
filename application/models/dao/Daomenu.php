08eb9de97bec3b588a7dd4900914f14966a32a60
darapenhchet <darapenhchet@gmail.com>
2015-08-06 23:21:10 +0700

    Change Language in Menu

--- application/models/dao/Daomenu.php
+++ application/models/dao/Daomenu.php
@@ -35,7 +35,7 @@ class DaoMenu extends CI_Model{
 		$this->db->join('MENUS B', 'A.subof=B.menuid', 'left');
 		$this->db->join('MENUDETAIL C', 'A.menuid=C.menuid');
 		//$this->db->group_by('A.subof,A.ordering');
-		$this->db->where('C.languageid',1);
+		$this->db->where('C.languageid',2);
 		//$this->db->order_by('A.subof,A.ordering');
 		$query = $this->db->get();
 		return $query->result();
@@ -87,7 +87,7 @@ class DaoMenu extends CI_Model{
 		}
 	}
 
-	public function listTopMenu($languageid=1){
+	public function listTopMenu($languageid=2){
 		$this->db->select('A.menuid, B.title, A.level');
 		$this->db->from('MENUS A');
 		$this->db->join('MENUDETAIL B','A.menuid = B.menuid');
@@ -108,73 +108,7 @@ class DaoMenu extends CI_Model{
 		$query = $this->db->get();
 		return $query->result();	
 	}
-
-	/*public function addMenu(DtoMenu $menu){
-		$subof=null;
-		if($menu->getSubof()!=''){$subof=$menu->getSubof();}
-		$data = array(
-				'title'		=> $menu->getTitle(),
-				'linkto'	=> $menu->getLinkto(),
-				'ordering'	=> $menu->getOrdering(),
-				'subof'		=> $subof
-		);
-		return $this->db->insert('MENUS', $data);
-	}
-
-	public function listMenu(){
-		$this->db->select('M1.menuid, M1.Title title, M1.linkto, M1.ordering, M2.title suboftitle');
-		$this->db->from('MENUS M1');
-		$this->db->join('MENUS M2', 'M1.subof=M2.menuid', 'left');
-		$query = $this->db->get();
-		return $query->result();
-	}
-
-	public function listTopMenu(){
-		$this->db->where('subof', null);
-		$query = $this->db->get('MENUS');
-		return $query->result();
-	}
-
-	public function getMenu($mid){
-		$this->db->where('menuid', $mid);
-		$query = $this->db->get('MENUS');
-		return $query->result();
-	}
-
-	public function updateMenu(DtoMenu $m){
-		$subof=null;
-		if($m->getSubof()!=''){$subof=$m->getSubof();}
-		$data = array(
-				'title'		=> $m->getTitle(),
-				'linkto'	=> $m->getLinkto(),
-				'ordering'	=> $m->getOrdering(),
-				'subof'		=> $subof
-		);
-		$this->db->where('menuid', $m->getMenuid());
-		return $this->db->update('MENUS', $data);
-	}
-
-	public function deleteMenu($mid){
-		$this->db->where('menuid', $mid);
-		return $this->db->delete('MENUS');
-	}
-
-	public function deleteMenuByTitle($mTitle){
-		$this->db->where("replace(LOWER(title),' ','')=", $mTitle);
-		if($this->db->delete('MENUS')){
-			return true;
-		}else{
-			return false;
-		}
-	}
-
-	public function findAllMenus(){
-		$this->db->select($this->input->cookie("LANGUAGE").'title AS TITLE');
-		$this->db->from("MENUS");
-		$query = $this->db->get();
-		return $query->result_array();
-	}*/
-
+	
 }
 
 ?>
