b89f0648a0b8e65f52fce26c19ddcceb10341950
bunrong <bunrongleang@gmail.com>
2015-08-08 23:02:26 +0700

    aa

--- application/language/khmer/smartmart_lang.php
+++ application/language/khmer/smartmart_lang.php
@@ -3,8 +3,8 @@
 $lang['hello']= 'សួស្តី';
 $lang['home']= 'ទំព័រដើម';
 $lang['product']= 'ផលិតផល';
-$lang['graduation']= 'Graduation';
-$lang['gift']= 'Gift';
+$lang['graduation']= 'បញ្ចប់ការសិក្សា';
+$lang['gift']= 'កាដូរ';
 $lang['information']= 'ពត៌មាន';
 $lang['contact']= 'ទំនាក់ទំនង';
 $lang['lang_abbr'] = "kh";
