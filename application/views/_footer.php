fbb4d1f0d7a43cec2f952bc6ee014cd073347c22
vuthea <chheang.vuthea@gmail.com>
2015-08-08 14:55:45 +0700

    rate done

--- application/views/_footer.php
+++ application/views/_footer.php
@@ -203,3 +203,33 @@
 <!-- Footer Scripts
 ============================================= -->
 <script type="text/javascript" src="<?php echo base_url('public/style_front/js/functions.js');?>"></script>
+
+<script type="text/javascript">(function(d, t, e, m){
+    
+    // Async Rating-Widget initialization.
+    window.RW_Async_Init = function(){
+                
+        RW.init({
+            huid: "221456",
+            uid: "0c7735d3de292b0250869c1816826be2",
+            source: "website",
+            options: {
+                "size": "medium",
+                "style": "oxygen",
+                "isDummy": false
+            } 
+        });
+        RW.render();
+    };
+        // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+        l = d.location, ck = "Y" + t.getFullYear() + 
+        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+    if (d.getElementById(id)) return;              
+    rw = d.createElement(e);
+    rw.id = id; rw.async = true; rw.type = "text/javascript";
+    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+    s.parentNode.insertBefore(rw, s);
+    }(document, new Date(), "script", "rating-widget.com/"));</script>
