6eb6a070ece695e2b8a7e91ec6550de059df33a0
darapenhchet <darapenhchet@gmail.com>
2015-08-18 23:53:36 +0700

    Disable Query Logging

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -12,12 +12,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$hook['post_controller'] = array(     // 'post_controller' indicated execution of hooks after controller is finished
+/*$hook['post_controller'] = array(     // 'post_controller' indicated execution of hooks after controller is finished
     'class' => 'Db_log',             // Name of Class
     'function' => 'logQueries',     // Name of function to be executed in from Class
     'filename' => 'db_log.php',    // Name of the Hook file
     'filepath' => 'hooks'         // Name of folder where Hook file is stored
-);
+);*/
 
 $hook['post_controller_constructor'] = array(  // 'post_controller_constrcutor' indicated execution of hooks after controller is finished
     'class' => 'Language_loader',             // Name of Class
